8ce5e48a966b9e82eb000c7586b6f2a1bdd62d95
Simone Conradi <47389241+conradis@users.noreply.github.com>
2019-03-26 20:33:03 +0100

    Add files via upload

--- Lezione3/opencv_motiondetect.py
+++ Lezione3/opencv_motiondetect.py
@@ -19,7 +19,6 @@ firstFrame = None
 while True:
 	# cattura del frame corrente
 	_, frame = vs.read()
-	frame = frame if args.get("video", None) is None else frame[1]
 	text = "Unoccupied"
 
 	# se il frame è vuoto allora si è alla fine del video
@@ -35,7 +34,7 @@ while True:
 	if firstFrame is None:
 		firstFrame = gray
 		continue
-    # calcolo della differenza assoluta tra il primo frame e quello corrente
+  # calcolo della differenza assoluta tra il primo frame e quello corrente
 	frameDelta = cv2.absdiff(firstFrame, gray)
 	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
@@ -55,7 +54,7 @@ while True:
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		text = "Occupied"
 
-    # disegna testo e ora sull'immagine
+  # disegna testo e ora sull'immagine
 	cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
--- Lezione3/opencv_showim.py
+++ Lezione3/opencv_showim.py
@@ -14,3 +14,4 @@ while(1):
 
 #chiusura della finestra
 cv2.destroyAllWindows()
+
